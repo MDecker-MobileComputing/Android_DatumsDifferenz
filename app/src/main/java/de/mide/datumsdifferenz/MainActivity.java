4c23457f62926adb4368869f35fa93875e1449c4
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-09-26 23:10:06 +0200

    Methode berechneAnzahlTag() dazu

--- app/src/main/java/de/mide/datumsdifferenz/MainActivity.java
+++ app/src/main/java/de/mide/datumsdifferenz/MainActivity.java
@@ -1,7 +1,18 @@
 package de.mide.datumsdifferenz;
 
+
+import android.widget.DatePicker;
+import android.widget.DatePicker.OnDateChangedListener;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+
+import java.util.Date;
+
 
 /**
  * App zur Demonstration Event-Handling für UI-Element "DatePicker".
@@ -16,6 +27,31 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * Berechnet Anzahl Tage zwischen heute und dem als Argument übergebenen Datum.<br>
+     * <br>
+     * Verwendet die Bibliothek "joda-time", siehe Dependency-Eintrag in Datei
+     * {@code app/build.gradle}.
+     *
+     * @param jahr Jahreszahl, z.B. 2021
+     * @param monat 1-basierter Monat (1=Januar, 12=Dezember)
+     * @param tagImMonat Tag im Monat, 1 bis 31 (je nach Anzahl Tage in {@code monat})
+     * @return Anzahl Tage zwischen heutigem Datum und Datum, das durch die Argumente
+     *         beschrieben wird. Zahl ist negativ wenn das Datum in der Vergangenheit
+     *         liegt.
+     */
+    private int berechneAnzahlTag(int jahr, int monat, int tagImMonat) {
+
+        LocalDate heuteLocalDate   = new LocalDate( new Date() );
+        LocalDate anderesLocalDate = LocalDate.parse(jahr + "-" + monat + "-" + tagImMonat);
+
+        // Klasse Days ist aus joda-time
+        Days days = Days.daysBetween(heuteLocalDate, anderesLocalDate);
+
+        return days.getDays();
     }
 
 }
