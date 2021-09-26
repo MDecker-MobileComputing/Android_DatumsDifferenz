239d45b675cb52208ba522ff5d7fb40e0fd0cbec
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-09-26 21:47:39 +0200

    Projekt nach Erzeugung mit Wizard und RelativeLayout mit DatePicker.

---
+++ app/src/main/java/de/mide/datumsdifferenz/MainActivity.java
@@ -0,0 +1,16 @@
+package de.mide.datumsdifferenz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
