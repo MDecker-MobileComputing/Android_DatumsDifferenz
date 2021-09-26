919ae96954a99aca0d03ee7748c8db442027b7b5
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-09-26 21:49:32 +0200

    MainActivity gefixt

--- app/src/main/java/de/mide/datumsdifferenz/MainActivity.java
+++ app/src/main/java/de/mide/datumsdifferenz/MainActivity.java
@@ -1,9 +1,14 @@
 package de.mide.datumsdifferenz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * App zur Demonstration Event-Handling für UI-Element "DatePicker".
+ * <br><br>
+ *
+ * This project is licensed under the terms of the BSD 3-Clause License.
+ */
 public class MainActivity extends Activity {
 
     @Override
